099b135ede6d73ca133e5a05fc2dc635a0833f9f
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-24 22:30:01 +0600

    my reviews added

--- src/components/Dashboard/AddReview.js
+++ src/components/Dashboard/AddReview.js
@@ -31,13 +31,6 @@ const AddReview = () => {
                 <input className='mt-2 btn btn-primary' value="Add Reviews" type='submit' />
             </form>
 
-            <div class="rating">
-                <input type="radio" name="rating-1" class="mask mask-star" />
-                <input type="radio" name="rating-1" class="mask mask-star" checked />
-                <input type="radio" name="rating-1" class="mask mask-star" />
-                <input type="radio" name="rating-1" class="mask mask-star" />
-                <input type="radio" name="rating-1" class="mask mask-star" />
-            </div>
         </div>
     );
 };
--- src/components/Dashboard/MyReview.js
+++ src/components/Dashboard/MyReview.js
@@ -1,9 +1,41 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import '../Home/Tools/Tool.css'
 const MyReview = () => {
+
+    const [user, loading, error] = useAuthState(auth);
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/review?review=${user.email}`)
+                .then(res => res.json())
+                .then(data => setReviews(data))
+        }
+    }, []);
+
     return (
         <div>
-            <h2>Reviews</h2>
+            <h2>My Reviews : {reviews.email}</h2>
+
+            {
+                reviews.map(r => <div key={r._id}>
+
+                    <div className='mt-0'>
+                        <div className="  w-96 bg-base-100 shadow-xl mt-5">
+                            <figure><img src={r.img} alt="tool" className='tool-img' /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{r.name}</h2>
+                                <p>{r.about}</p>
+                                <p>Ratings: {r.ratings} (Out of 5)</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>)
+            }
         </div>
     );
 };
