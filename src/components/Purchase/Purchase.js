3a36587002623db81af74b3b8559a2cd16111ae0
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-24 15:04:25 +0600

    order send to database

--- src/components/Purchase/Purchase.js
+++ src/components/Purchase/Purchase.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
 
 const Purchase = () => {
@@ -12,8 +13,8 @@ const Purchase = () => {
     const [count, setCount] = useState(parseInt(0));
 
     const handleDecrement = () => {
-       
-        if (product.minQuantity > 0 ) {
+
+        if (product.minQuantity > 0) {
             setCount(product.minQuantity = (parseInt(product.minQuantity) - 1));
         }
 
@@ -40,7 +41,29 @@ const Purchase = () => {
 
     const handleOrder = event => {
         event.preventDefault();
-        const name = event.target.name.value;
+
+        const order = {
+            orderId: product._id,
+            order: product.name,
+            customer: user.email,
+            customerName: user.displayName,
+            phone: event.target.phone.value,
+            address: event.target.address.value,
+            info: event.target.info.value,
+            price: totalPrice
+        }
+
+        fetch('http://localhost:5000/order',{
+            method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(order)
+        })
+        .then(res=>res.json())
+        .then(data => {
+            toast('data is set');
+        })
     }
 
     const totalPrice = ((product.minQuantity) * (product.price));
@@ -61,14 +84,14 @@ const Purchase = () => {
                 <input type="number" name='phone' placeholder="Your Phone Number" className="input input-bordered md:ml-[500px] input-md w-full mt-4 max-w-sm" />
                 <br />
 
-              <div>
-                  <p className='md:ml-[500px] w-full'>Min Quantity</p>
-              <div className="md:ml-[500px] w-full mt-4 border  max-w-sm flex justify-between items-center">
-                    <button type='button' className='text-center pl-4 text-red-800 font-bold text-3xl' onClick={handleDecrement}> - </button>
-                    <div className='pl-4 text-2xl font-bold'>{product.minQuantity}</div>
-                    <button className='pr-4 text-green-800 font-bold text-3xl' onClick={handleIncrement} type='button'> + </button>
+                <div>
+                    {/* <p className='md:ml-[500px] w-full'>Min Quantity</p> */}
+                    <div className="md:ml-[500px] w-full mt-4 border  max-w-sm flex justify-between items-center">
+                        <button type='button' className='text-center pl-4 text-red-800 font-bold text-3xl' onClick={handleDecrement}> - </button>
+                        <div className='pl-4 text-2xl font-bold'>{product.minQuantity}</div>
+                        <button className='pr-4 text-green-800 font-bold text-3xl' onClick={handleIncrement} type='button'> + </button>
+                    </div>
                 </div>
-              </div>
 
 
                 {/* <input type="text" name='quantity' onChange={increaseCount} placeholder="quantity"  className="input input-bordered md:ml-[500px] input-md w-full mt-4 max-w-sm" /> */}
